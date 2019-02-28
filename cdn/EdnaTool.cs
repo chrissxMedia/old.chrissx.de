3dff12cfa97bd36ff453d0d05e3b0a6096536b6c
root <root@chrissx.ga>
2019-02-28 21:48:04 +0100

    initial commit (without the backup now)

---
+++ cdn/EdnaTool.cs
@@ -0,0 +1,19 @@
+﻿//(c) 2018 by chrissx
+//if you use this, please give credit
+
+using static System.Console;
+using static System.IO.File;
+using static System.IO.Path;
+
+namespace E{class P{static void Main(){
+	Write("Video dictionary: ");
+	string[] l = ReadAllLines(ReadLine());
+	Write("Video directory: ");
+	string vd = ReadLine();
+	Write("Output directory: ");
+	string od = ReadLine();
+	int i = 0;
+	while (i < l.Length)
+		Copy(Combine(vd, l[i++]),
+			Combine(od, l[i++]));
+}}}
---
+++ cdn/apache_transfer.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using static System.Console;
+
+namespace apache_transfer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("From: ");
+            string from = ReadLine();
+            Write("To: ");
+            string to = ReadLine();
+            Process.Start(new ProcessStartInfo("scp", $"-4 -c aes256-ctr -C -p -P 22 {from} pi@192.168.2.104:{to}")
+            {
+                CreateNoWindow = false,
+                RedirectStandardError = false,
+                RedirectStandardInput = false,
+                RedirectStandardOutput = false,
+                UseShellExecute = true
+            });
+        }
+    }
+}
