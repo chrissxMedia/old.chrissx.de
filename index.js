c054dedbb4d15047a8d9a8694b1c403b3f8d35c6
chrissx@ThinkBadly <chrissx@chrissx.ga>
2019-07-21 23:32:00 +0200

    completely abolished that pwa idea, because it was just stupid

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-function cache_clear() {
-	caches.open('chrissx.ga').then(function(cache) {
-		cache.delete('/');
-		cache.delete('/index.html');
-		cache.delete('/style/dark.css');
-		cache.delete('/manifest.json');
-		cache.delete('/favicon.ico');
-		cache.delete('/logo/4000.png');
-		cache.delete('/logo/4096.png');
-		cache.delete('/logo/256.ico');
-		cache.delete('/logo/256t.ico');
-		cache.delete('/sw_init.js');
-		cache.delete('/index.js');
-		navigator.serviceWorker.getRegistrations().then(
-			function(registrations) {
-				for(let registration of registrations) {
-					registration.unregister();
-				}
+caches.open('chrissx.ga').then(function(cache) {
+	cache.delete('/');
+	cache.delete('/index.html');
+	cache.delete('/style/dark.css');
+	cache.delete('/manifest.json');
+	cache.delete('/favicon.ico');
+	cache.delete('/logo/4000.png');
+	cache.delete('/logo/4096.png');
+	cache.delete('/logo/256.ico');
+	cache.delete('/logo/256t.ico');
+	cache.delete('/sw_init.js');
+	cache.delete('/index.js');
+	navigator.serviceWorker.getRegistrations().then(
+		function(registrations) {
+			for(let registration of registrations) {
+				registration.unregister();
 			}
-		);
-		console.log("cleared cache.");
-		location.reload();
-	});
-}
+		}
+	);
+	console.log("ERASED THAT CACHE!");
+});
